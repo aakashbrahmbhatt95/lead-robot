65e3b970cb0f9964a56c33f409e3b29c84af664d
jaydeep <anvesh@mobilefirstapplications.com>
2024-06-04 13:56:14 +0530

    create custom button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import Layout from "@/components/Layout";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import useLogout from "@/hooks/useLogout";
+import { Plus } from "@phosphor-icons/react";
+import { CustomButton } from "@/components/ui/CustomButton";
 
 const Home = () => {
-  const { logout } = useLogout()
+  const { logout } = useLogout();
   return (
     <main className="">
       <Layout>
@@ -21,9 +23,13 @@ const Home = () => {
               <Link href="/ui-components" className="text-blue-500 underline">
                 UI Components
               </Link>
-              <Button onClick={() => logout()} className="text-blue-500 underline">
-                Logout
-              </Button>
+              <Button onClick={() => logout()}>Logout</Button>
+              <CustomButton variant={"outline"} icon={Plus} iconPosition="left">
+                Add Item
+              </CustomButton>
+              <CustomButton icon={Plus} iconPosition="right" variant="outline">
+                Add Item
+              </CustomButton>
             </div>
           </div>
         </main>
