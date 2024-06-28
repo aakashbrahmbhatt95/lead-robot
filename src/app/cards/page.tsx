aec199fdead994324ce45a20c167c27947ffe372
jaydeep <anvesh@mobilefirstapplications.com>
2024-06-28 17:38:10 +0530

    Update page.tsx

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -9,13 +9,24 @@ import AskCard from "@/components/AskCard";
 
 const Cards: React.FC = () => {
   const components = useAppSelector((state) => state.components.components);
+  const handleDelete = (id: string) => {
+    console.log(`Delete card with id: ${id}`);
+  };
+
+  const handleCopy = (id: string) => {
+    console.log(`Copy card with id: ${id}`);
+  };
 
   return (
     <main className="">
       <Layout>
         <main className="flex justify-center items-center bg-[#f4f4f5] min-h-screen">
           <div className="flex flex-col gap-10 items-center justify-center bg-white w-[578px] rounded-[24px] p-8">
-            <ParentTaskCard />
+            <ParentTaskCard
+              id="1"
+              handleDelete={handleDelete}
+              handleCopy={handleCopy}
+            />{" "}
           </div>
         </main>
       </Layout>
