64fafab19b776bee0488de97aa91f384cd86c314
rajath-mobilefirst <rajath.mobilefirst@gmail.com>
2024-11-11 12:10:26 +0530

    fixed css issues of multiselect

--- src/components/Segments/MultiSelect.tsx
+++ src/components/Segments/MultiSelect.tsx
@@ -17,11 +17,11 @@ export const MultiSelect = ({ field, options, setFieldValue }: any) => {
 
   return (
     <Popover>
-      <PopoverTrigger className="flex justify-between mt-1 border border-gray px-3 py-2 rounded-lg">
+      <PopoverTrigger className="flex justify-between mt-1 border border-gray px-3 py-2 rounded-md w-[250px]">
         {field.value.length > 0 ? field.value.join(", ") : "Select"}
         <ChevronDown />
       </PopoverTrigger>
-      <PopoverContent className="w-[311px] p-2 bg-white shadow-md rounded-lg">
+      <PopoverContent className="w-[250px] p-2 bg-white shadow-md rounded-lg">
         {options.map((option: any) => (
           <div key={option} className="flex items-center gap-3 mt-2">
             <Checkbox
