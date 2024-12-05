c2afe1c5c59117ae504769393822d35775eb8286
rajath-mobilefirst <rajath.mobilefirst@gmail.com>
2024-12-05 15:23:44 +0530

    fixed login issue

--- src/app/login/googleLogin.tsx
+++ src/app/login/googleLogin.tsx
@@ -1,67 +1,72 @@
 import { Button } from "@/lib/ui/button";
-import {
-  GoogleOAuthProvider,
-  useGoogleLogin,
-  TokenResponse,
-} from "@react-oauth/google";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import Image from "next/image";
 import googleicon from "../../../public/Google.svg";
 import { HttpUtil } from "@/utils/http-util";
 import { BASE_URL, PROVIDER_TOKEN } from "@/utils/apiConstants";
+import { toast } from "react-toastify";
+import { setCookie } from "cookies-next";
+import { SESSION_KEY, TOKEN_KEY } from "@/utils/constants";
+import { useRouter } from "next/navigation";
 
 const GoogleAuthButton = () => {
-  const handleGoogleAuth = (response: TokenResponse) => {
-    const { access_token } = response; // Extract access token from response
-    if (access_token) {
+  const router = useRouter();
+  const handleGoogleAuth = (credentialResponse: any) => {
+    const { credential: id_token } = credentialResponse;
+
+    if (id_token) {
       const payload = {
         provider: "google",
         process: "login",
         token: {
-          client_id:
-            "1088482495941-e4jgbnmnda3q1k498aa9rp1qrljevit8.apps.googleusercontent.com",
-          access_token, // Access token from Google response
-          id_token: "",
+          client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+          access_token: "", // Not required in this flow
+          id_token,
         },
       };
 
       HttpUtil.makePOST(`${BASE_URL}${PROVIDER_TOKEN}`, payload)
         .then((res) => {
           console.log("Response:", res);
+          toast.success("User Logged In Successfully");
+          setCookie(TOKEN_KEY, res.data.meta.access_token);
+          setCookie(SESSION_KEY, res.data.meta.session_token);
+          router.push("/campaigns");
         })
         .catch((err: any) => {
           console.error("Error:", err);
         });
     } else {
-      console.error("No access token found in the response.");
+      console.error("No ID token found in the response.");
     }
   };
 
-  const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-      console.log("tokenResponse", tokenResponse);
-      handleGoogleAuth(tokenResponse);
-    },
-    onError: () => {
-      console.error("Google Login Failed");
-    },
-  });
-
   return (
-    <Button
-      variant="outline"
-      type="button"
-      className="rounded-[20px] flex items-center gap-4 w-full"
-      onClick={() => login()}
+    // @ts-ignore
+    <GoogleLogin
+      onSuccess={handleGoogleAuth}
+      onError={() => {
+        console.error("Google Login Failed");
+      }}
+      useOneTap
     >
-      <Image src={googleicon} alt="Google Icon" className="w-6 h-6" />
-      Continue with Google
-    </Button>
+      <Button
+        variant="outline"
+        type="button"
+        className="rounded-[20px] flex items-center gap-4 w-full"
+      >
+        <Image src={googleicon} alt="Google Icon" className="w-6 h-6" />
+        Continue with Google
+      </Button>
+    </GoogleLogin>
   );
 };
 
 const GoogleAuthContainer = () => {
   return (
-    <GoogleOAuthProvider clientId="1088482495941-e4jgbnmnda3q1k498aa9rp1qrljevit8.apps.googleusercontent.com">
+    <GoogleOAuthProvider
+      clientId={`${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`}
+    >
       <GoogleAuthButton />
     </GoogleOAuthProvider>
   );
