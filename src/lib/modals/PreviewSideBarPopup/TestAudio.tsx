6f239937e9bf5e12aa815e4972dd302829a19b6b
rajath-mobilefirst <rajath.mobilefirst@gmail.com>
2024-12-06 13:24:06 +0530

    added livekit

--- src/lib/modals/PreviewSideBarPopup/TestAudio.tsx
+++ src/lib/modals/PreviewSideBarPopup/TestAudio.tsx
@@ -6,9 +6,9 @@ const TestAudio = () => {
     <div className="h-[80vh] flex flex-col items-center justify-center gap-5">
       <Mic width={80} height={80} color="lightgray" />
       <p className="text-lg font-medium">Test your agent</p>
-      <Button type="button" className="text-lg font-medium" variant="outline">
+      <a href="/livekit-audio" className="text-lg font-medium" target="_blank">
         Test
-      </Button>
+      </a>
     </div>
   );
 };
