9867b5f54c66cd90bee6683b05272bbfc1f684b0
rajath-mobilefirst <rajath.mobilefirst@gmail.com>
2024-12-17 18:00:21 +0530

    fixed livekit issue

--- src/lib/modals/PreviewSideBarPopup/TestAudio.tsx
+++ src/lib/modals/PreviewSideBarPopup/TestAudio.tsx
@@ -11,10 +11,10 @@ const TestAudio = ({
       className="h-[80vh] flex items-center justify-center"
     >
       <div className="flex flex-col items-center justify-center gap-5 p-[45px] rounded-[50%] bg-[#f0efef] cursor-pointer" 
-      // onClick={() => {
-      //   setIsVoiceAssitantPopup(true);
-      //   setIsPreviewSideBarPopup(null);
-      // }}
+      onClick={() => {
+        setIsVoiceAssitantPopup(true);
+        setIsPreviewSideBarPopup(null);
+      }}
       >
         <Mic width={80} height={80} color="lightgray" />
         <p className="text-lg font-medium">Test your agent</p>
